5d40e8d4bee43b4e76219bb50e1b52eeea2b1169
opilane <opilane@HP-CZC2349HT3>
2023-12-11 10:26:29 +0200

    Add project files.

---
+++ LOGITpv23_valeriiashurov.py
@@ -0,0 +1,25 @@
+arv1=float(input("Arv1: "))
+t=input("Tehe: ")
+arv2=float(input("Arv2: "))
+vastus=eval(str(arv1)+t+str(arv2))
+print(str(arv1)+t+str(arv2)+"="+str(vastus))
+print(arv1,t,arv2,"=",vastus,sep="") #separator - razdelitel'
+print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
+
+
+
+
+
+
+
+
+
+#print("Tere maailm!".center(75,"-"))
+#nimi=input("Mis on sinu nimi?").capitalize() #python->Python
+#print("Tere "+nimi+"!")
+#print("Tere",nimi+"!")
+#print("Tere {0}!".format(nimi))
+#vanus=int(input("Kui vana sa oled?"))               #"21" !=21
+#print("Tere {0}! Sa oled {1} aastat vana".format(nimi,vanus))
+#print("Muutuja nimi=",nimi,type(nimi))
+#print("Muutuja vanus=",vanus,type(vanus))
