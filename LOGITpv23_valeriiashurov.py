e7c524be9464e62e76e905e251017cdd9d904e56
opilane <opilane@HP-CZC2349HT3>
2023-12-11 11:29:16 +0200

    asd123

--- LOGITpv23_valeriiashurov.py
+++ LOGITpv23_valeriiashurov.py
@@ -1,10 +1,23 @@
-arv1=float(input("Arv1: "))
-t=input("Tehe: ")
-arv2=float(input("Arv2: "))
-vastus=eval(str(arv1)+t+str(arv2))
-print(str(arv1)+t+str(arv2)+"="+str(vastus))
-print(arv1,t,arv2,"=",vastus,sep="") #separator - razdelitel'
-print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
+#3
+from math import *
+from random import *
+from xml.etree.ElementTree import PI
+#kokku=randint(1,666)
+#print("Laual peal on",kokku,"kommid. Mitu tahad süüa?")
+#kommid=int(input())
+#kokku-=kommid 
+#print("Nüüd kokku on",kokku)
+
+
+#2
+
+#arv1=float(input("Arv1: "))
+#t=input("Tehe: ")
+#arv2=float(input("Arv2: "))
+#vastus=eval(str(arv1)+t+str(arv2))
+#print(str(arv1)+t+str(arv2)+"="+str(vastus))
+#print(arv1,t,arv2,"=",vastus,sep="") #separator - razdelitel'
+#print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
 
 
 
@@ -12,7 +25,7 @@ print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
 
 
 
-
+#1
 
 #print("Tere maailm!".center(75,"-"))
 #nimi=input("Mis on sinu nimi?").capitalize() #python->Python
@@ -23,3 +36,32 @@ print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
 #print("Tere {0}! Sa oled {1} aastat vana".format(nimi,vanus))
 #print("Muutuja nimi=",nimi,type(nimi))
 #print("Muutuja vanus=",vanus,type(vanus))
+
+
+
+
+#vanus=18
+#eesnimi="Jaak"
+#pikkus=16.5
+#kas_käib_koolis=True
+
+#print("Muutuja vanus=",vanus,"on",type(vanus))
+#print("Muutuja eesnimi=",eesnimi,"on",type(eesnimi))
+#print("Muutuja pikkus=",pikkus,"on",type(pikkus))
+
+
+
+
+#4
+
+#u=float(input("Ümbermõõt: ")) #L=pi*2*r=pi*d
+#d=round(u/pi,2) 
+#print("Läbimõõt =",d)
+
+#5
+
+from math import 
+#a=float(input("pikkus: "))
+#b=float(input("laius: "))
+#d=sqrt(a**2+b**2)
+#print("Diagonaal=",d,"m") 
