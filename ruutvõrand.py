0b963d7f2a51a326f04c5d195b7d83d7d546a9cc
valeriin0ry <153493936+valeriin0ry@users.noreply.github.com>
2024-01-15 11:45:59 +0200

    Add files via upload

--- ruutvõrand.py
+++ ruutvõrand.py
@@ -53,33 +53,60 @@
 #        print("Viga andmetüübiga")
 
     #1 задача (легкая до ужаса)
-number = float(input("Sisestage number: "))
-if number > 0:
-    print("Arv on positiivne")
-    if number % 2 == 0:
-        print("Paarisarv")
-    else:
-        print("Paaritu number")
-elif number < 0:
-    print("Negatiivne arv")
-else:
-    print("Neutraalne")
+#number = float(input("Sisestage number: "))
+#if number > 0:
+#    print("Arv on positiivne")
+#    if number % 2 == 0:
+#        print("Paarisarv")
+#    else:
+#        print("Paaritu number")
+#elif number < 0:
+#    print("Negatiivne arv")
+#else:
+#    print("Neutraalne")
 
 
-    #2
+#    #2
 
+#from math import *
+#a,b,c=map(float,input("a,b,c:").split())
+#if a>0 and b>0 and c>0:
+#    if a+b+c==180:
+#        print("Kolmnurk")
+#        if a==b==c:
+#            print("võrdkülgne")
+#        elif a==b or b==c or a==c:
+#            print("võrdhaarne")
+#        else:
+#            print("erikülgne")
+#    else:
+#        print("Nurgad")
+#else:
+#    print("<0")
+
+
+    #3
 from math import *
-a,b,c=map(float,input("a,b,c:").split())
-if a>0 and b>0 and c>0:
-    if a+b+c==180:
-        print("Kolmnurk")
-        if a==b==c:
-            print("võrdkülgne")
-        elif a==b or b==c or a==c:
-            print("võrdhaarne")
+v=input("Kas tahad 1-7 numbrist saada päeva nimetus?")
+if v.lower()=="jah":
+    try:
+        nr=int(input("Päeva number: "))
+        if nr==1:
+            p="esmaspäev"
+        elif nr==2:
+            p="teisipäev"
+        elif v==3:
+            p="kolmapäev"
+        elif nr==4:
+            p="neljapäev"
+        elif nr==5:
+            p="reede"
+        elif nr==6:
+            p="laupäev"
+        elif nr==7:
+            p="pühapäev"
         else:
-            print("erikülgne")
-    else:
-        print("Nurgad")
-else:
-    print("<0")
+            p="on vaja 1-7"
+        print(p)
+    except:
+        print("Viga")
