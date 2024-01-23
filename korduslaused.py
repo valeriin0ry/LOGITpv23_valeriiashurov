e6e5cdf899519df6cfb1404a4a34e5788fd529ba
valeriin0ry <153493936+valeriin0ry@users.noreply.github.com>
2024-01-23 10:22:17 +0200

    Add files via upload

--- korduslaused.py
+++ korduslaused.py
@@ -56,24 +56,101 @@ from random import *
 
 
 #3
-k=0
-while True:
-    k+=1
-    a=randint(1,50)
-    b=randint(1,50)
-    p=0
-    v=-1000
-    while p!=3:
-        p+=1
-        v=int(input("Millega võrdub {0}+{1}= ".format(a,b)))
-        if v==a+b:
-            print("Tubli!")
-            break
-        else:
-            print("Mõtle veel!")
-    print("{0}+{1}={2}".format(a,b,a+b))
+#k=0
+#while True:
+#    k+=1
+#    a=randint(1,50)
+#    b=randint(1,50)
+#    p=0
+#    v=-1000
+#    while p!=3:
+#        p+=1
+#        v=int(input("Millega võrdub {0}+{1}= ".format(a,b)))
+#        if v==a+b:
+#            print("Tubli!")
+#            break
+#        else:
+#            print("Mõtle veel!")
+#    print("{0}+{1}={2}".format(a,b,a+b))
 
-    if k==5:break
+#    if k==5:break
+
+    #Таблица умножения на 5 (9)
+    #from random import *
+    #from datetime import *
+    
+    #korrutamine=["5"]
+    #arv:["1","2","3","4","5","6","7","8","9","10"]
+    #for i in range(10):
+    #    tulemus = int(arv[i]) * 5
+    #    print(f"{arv[i]} * 5 = {tulemus}")
+
+
+    #15
+#katsed = 0
+#while True:
+#    vastus = input("Osta elevant ära! Kirjuta 'elevant': ")
+#    katsed += 1
+
+#    if vastus.lower() == 'elevant':
+#        print(f"Õige! Ostsid elevanti ära {katsed} katsega.")
+#        break
+#    else:
+#        print("Vale vastus. Proovi uuesti.")
+
+#    #7
+#    from random import *
+#    for i in range(5):
+#        number=randint(0,9)
+#        print(number, end="")
+#    print()
+
+#8
+#paaris=0
+#paaritu=0
+#for i in range(1,101):
+#    if i%2==0:
+#        print(f"{i}-paaris")
+#        paaris+=1
+#    else:
+#        print(f"{i}-paaritu")
+#        paaritu+=1
+
+#print(f"Paarisarvude arv: {paaris}")
+#print(f"Paaritute arvude arv: {paaritu}")
+
+#kakaya to zada4a
+#from random import *
+#number=randint(1,100)
+#katsed=3
+#while katsed>0:
+#    külaline=int(input("Угадайте число от 1 до 100: "))
+#    if külaline == number:
+#        print("Поздравляю, вы угадали число!")
+#        break
+#    else:
+#        katsed -= 1
+#        print(f"Неверно. У вас осталось {katsed} попыток.")
+#        if katsed == 0:
+#            print(f"Извините, вы использовали все попытки. Загаданное число было {number}")
+#            veelkord = input("Хотите ли вы угадать?: ").lower()
+#            if veelkord.lower()=="нет":
+#                break
+#            else:
+#                katsed=3
+
+
+
+    #13
+print("Arv Ruut Kuup")
+print()
+print("-------------")
+print()
+
+for i in range(1, 11):
+    ruut = i ** 2
+    kuup = i ** 3
+    print(f"{i:2}  {ruut:2}  {kuup:3}")
 
 
 
