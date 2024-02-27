c9597c08ec5f86f69fdc63bf929e956698769349
valeriin0ry <153493936+valeriin0ry@users.noreply.github.com>
2024-02-27 10:52:11 +0200

    Add files via upload

--- funktsioonidekasutamine.py
+++ funktsioonidekasutamine.py
@@ -2,17 +2,26 @@ from minuomamodule import *
 from random import *
 a=[] 
 b=[]
+#(1) arithmetic() funktsiooni kasutamine
+v=arithmetic(float(input("Arv1:")),float(input("Arv2:")),input("Operatsioon:"))
+print(v)
 
-for i in range(5):
-    a.append(randint(-20,20))
-for i in range(1,4):
-    arv=int(input(f"{i}. arv"))
-    b.append(arv)
-print(a)
-print(b)
-print()
-Suurim_element(a,b)
+#(2) liigaasta
+vastus=is_year_leap(int(input("Aasta: ")))
+if vastus:
+    print("Liigaasta")
+else:
+    print("Tavaline aasta")
 
+#for i in range(5):
+#    a.append(randint(-20,20))
+#for i in range(1,4):
+#    arv=int(input(f"{i}. arv"))
+#    b.append(arv)
+#print(a)
+#print(b)
+#print()
+#Suurim_element(a,b)
 
 
 
--- minuomamodule.py
+++ minuomamodule.py
@@ -1,3 +1,6 @@
+from pickle import TRUE
+
+
 def Korrutis(arv1:float,arv2:float,arv3:float,arv4=1.0)->float:
     """Funktsioon tagastab 4 arvude korrutis, arvud sisestab kasutaja. Vaikimisi arv4=1.0. Tulemus tagastatakse float formaadis
 
@@ -20,3 +23,39 @@ def Suurim_element(arvud1:list,arvud2:list)->any:
     suurim2=max(arvud2)
     suurim=max(suurim1,suurim2)
     print(suurim)
+#(1)
+def arithmetic(arv1:float,arv2:float,sumb:str)->any:
+    """Funktsion tagastab aritmeetiliste tehtede tulemused.
+    + - liitumine
+    - - lahutamine
+    * - korrutamine
+    / - jagamine
+    :param float arv1: ujukomaarv mis sisestab kasutaja
+    :param float arv2: ujukomaarv mis sisestab kasutaja
+    :param str sumb: sõne/tehe, mis sisestab kasutaja
+    """
+    if sumb=="+":
+        vastus=arv1+arv2
+    elif sumb=="-":
+        vastus=arv1-arv2
+    elif sumb=="*":
+        vastus=arv1*arv2
+    elif sumb=="/":
+        if arv2==0:
+            vastus="DIV/0"
+        else:
+            vastus=arv1/arv2
+    else:
+        vastus="Tundmatu operatsioon"
+    return vastus
+#(2)
+def is_year_leap(aasta:int)->bool:
+    """Funktsioon tagastab True, kui aasta on liigaasta või False, kui aasta on tavaline.
+    :param int aasta: Kasutaja sisestab aasta number
+    .rtype: bool
+    """
+    if aasta&4==0: 
+        vastus=True 
+    else:
+        vastus=False 
+    return vastus
