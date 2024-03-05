6841c16b82141d5348dd0a63f9bb02afd8c39ee5
valeriin0ry <153493936+valeriin0ry@users.noreply.github.com>
2024-03-05 11:35:27 +0200

    Add files via upload

--- aut_reg.py
+++ aut_reg.py
@@ -10,10 +10,19 @@ while True:
     elif vastus=="2":
         print("Autoriseerimine")
         autoriseerimine(kasutajanimed,salasõnad)
-    elif vastus=="3":
+    elif vastus == "3":
         print("Nime või parooli muutmine")
+        vastus = input("Kas muudame nime või parooli?: ")
+        if vastus == "nimi":
+            kasutajanimed = nimi_või_parooli_muurmine(kasutajanimed, "nimi")
+        elif vastus == "parool":
+            salasõnad = nimi_või_parooli_muurmine(salasõnad, "parool")
+        elif vastus == "mõlemad":
+            kasutajanimed = nimi_või_parooli_muurmine(kasutajanimed, "nimi")
+            salasõnad = nimi_või_parooli_muurmine(salasõnad, "parool")
     elif vastus=="4":
         print("Unustanud parooli taastamine")
+        parooli_taastamine(kasutajanimed, salasõnad)
     elif vastus=="5":
         print("Lõpetamine")
         break
--- minuomamodul.py
+++ minuomamodul.py
@@ -8,34 +8,38 @@ def registreerimine(kasutajad:list,paroolid:list)->any:
     :rtype: list,list
     """
     while True:
-        nimi=input("Mis on sinu nimi? ")
+        nimi = input("Mis on sinu nimi? ")
         if nimi not in kasutajad:
             while True:
-                parool=input("Mis on sinu parool? ")
-                flag_p=False 
-                flag_l=False
-                flag_u=False 
-                flag_d=False 
-                if len(parool)>=8:
-                    parool_list=list(parool)
+                parool = input("Mis on sinu parool? ")
+                flag_p = False
+                flag_l = False
+                flag_u = False
+                flag_d = False
+                if len(parool) >= 8:
+                    parool_list = list(parool)
                     for p in parool_list:
                         if p in punctuation:
-                            flag_p=True
+                            flag_p = True
                         elif p in ascii_lowercase:
-                            flag_l=True
+                            flag_l = True
                         elif p in ascii_uppercase:
-                            flag_u=True
+                            flag_u = True
                         elif p in digits:
-                            flag_d=True
+                            flag_d = True
                     if flag_p and flag_u and flag_l and flag_d:
-                        kasutajad.append(nimi) 
-                        paroolid.append(parool) 
-                    break 
+                        kasutajad.append(nimi)
+                        paroolid.append(parool)
+                        print("Kasutaja registreeritud!")
+                        break
+                    else:
+                        print("Nõrk salasõna! Salasõna peab sisaldama vähemalt üht suurt tähte, üht väikest tähte, ühte numbrit ja ühte erimärki.")
                 else:
-                    print("Nõrk salasõna!")
+                    print("Nõrk salasõna! Salasõna peab olema vähemalt 8 tähemärki pikk.")
             break
         else:
             print("Selline kasutaja on juba olemas!")
+    return kasutajad, paroolid
     return kasutajad, paroolid
 def autoriseerimine(kasutajad:list,paroolid:list):
     """Funktsioon kuvab ekraanile "Tere tulemast!" kui kasutaja on olemas nimekirjas
@@ -45,19 +49,59 @@ def autoriseerimine(kasutajad:list,paroolid:list):
     :param list kasutajad: ...
     :param list paroolid: ...
     """
-    p=0
+    p = 0
     while True: 
-        nimi=input("Sisesta kasutajanimi: ")
-        parool=input("Sisesta salasõna: ")
-        p+=1
+        nimi = input("Sisesta kasutajanimi: ") 
         if nimi in kasutajad:
-            if kasutajad.index(nimi)==paroolid.index(parool):
-                print(f"Tere tulemast! {nimi}")
-                break
-            else:
-                print("Vale nimi või salasõna!")
-                if p==5:
-                    print("Proovi uuesti 10 sek pärast")
-                    for i in range(10):
-                        sleep(1)
-                        print(f"On jäänud {10-i} sek")
+            while True:
+                parool = input("Sisesta salasõna: ")
+                p += 1
+                try:
+                    if kasutajad.index(nimi) == paroolid.index(parool):
+                        print(f"Tere tulemast! {nimi}")
+                        return
+                except ValueError:
+                    print("Vale nimi või salasõna!")
+                    if p == 5:
+                        print("Proovi uuesti 10 sekundi pärast")
+                        for i in range(10):
+                            sleep(1)
+                            print(f"On jäänud {10-i} sekundit")
+        else:
+            print("Kasutajat pole")
+def nimi_või_parooli_muurmine(list_:list, tüüp:str):
+    """
+    """
+    if tüüp=="nimi":
+        muutuja=input("Vana kasutajanimi: ")
+        uus_muutuja=input("Uus kasutajanimi: ")
+    elif tüüp=="parool":
+        muutuja=input("Vana parool: ")
+        uus_muutuja=input("Uus parool: ")
+    if muutuja in list_:
+        indeks=list_.index(muutuja)
+        muutuja=input("Uue nimi või parool: ")
+        list_[indeks]=uus_muutuja
+        print("Muudetud!")
+    else:
+        print("Sisestati vale kasutajanimi või parool")
+    return list_
+def parooli_taastamine(kasutajad:list, paroolid:list):
+    """
+
+    """
+    nimi = input("Sisesta oma kasutajanimi: ")
+    if nimi in kasutajad:
+        print("Kasutaja on olemas! Allpool on teie parool")
+        indeks = kasutajad.index(nimi)
+        parool = paroolid[indeks]
+        print(f"Sinu parool on: {parool}")
+        vastus=input("Kas te tahate parooli muuta? ")
+        if vastus.lower()=="jah":
+            uus_parool=input("Sisesta uus parool: ")
+            paroolid[indeks]=uus_parool
+            print("Muudetud!")
+        else:
+            print(f"Okei, siis siin on sinu parool! -> {parool}")
+    else:
+        print("Kasutaja pole")
